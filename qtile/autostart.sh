2671b9dcd2290f801cc03d359cd53c96495c3299
Pablo Alcañiz <93190637+pablo-alcaniz@users.noreply.github.com>
2025-02-24 16:41:29 +0100

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,32 +1,22 @@
-#!/bin/bash
-
+#pantalla
+/bin/python /home/pablo/.config/qtile/check_monitor.py
+#volumen
+volctl &
+#google drive
+insync start &
+#teclado 
+setxkbmap es
 #compositor de escritorio
-picom --experimental-backends &
-
-
+picom -b  &
 #iconos del sistema
 udiskie -t &
 nm-applet &
 blueman-applet &
-volctl &
 
+caffeine &
 #aplicaciones
-python3 /opt/thefanclub/overgrive/overgrive &
 optimus-manager &
 optimus-manager-qt &
 
 #fondo de pantalla
 nitrogen --restore  &
-
-#openfrezer
-#python3 /home/pablo/programas/OpenFreezer/indicator.py >/dev/null 2>&1 & exit
-
-#synaptics
-synclient VertScrollDelta=-60 &
-synclient TapButton1=1 &
-synclient PalmDetect=1 & 
-synclient PalmMinWidth=8 & 
-synclient PalmMinZ=300 &
-
-#dock 
-plank &
